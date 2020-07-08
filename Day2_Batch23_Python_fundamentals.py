77cabe647b7fac3a70e62172dca2fa73b33a558d
saikushwanth <67970353+saikushwanth@users.noreply.github.com>
2020-07-08 22:00:05 +0530

    Add files via upload

---
+++ Day2_Batch23_Python_fundamentals.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Day2 python class
+
+
+# In[2]:
+
+
+#rule1: No name spaces should be used while declaring the variable name 
+
+
+# In[3]:
+
+
+#rule2: You should not start a variable name with a number 
+
+
+# In[4]:
+
+
+#rule3: you should not use any special charater in the variable like @, #, $, % etc
+
+
+# In[5]:
+
+
+name = 'kushwanth'
+
+print(name)
+
+
+# In[7]:
+
+
+1name = kushwanth 
+
+print(1name)
+
+
+# In[9]:
+
+
+name1 = 'kush'
+
+print(name1)
+
+
+# In[10]:
+
+
+# single quote 
+
+
+# In[11]:
+
+
+name = 'kushwanth'
+
+print(name)
+
+
+# In[12]:
+
+
+#double quotes
+
+
+# In[13]:
+
+
+name = "kushwanth"
+
+print(name)
+
+
+# In[14]:
+
+
+#triple quotes
+
+
+# In[15]:
+
+
+name = """kushwanth"""
+
+print(name)
+
+
+# In[16]:
+
+
+type(name)
+
+
+# In[17]:
+
+
+type(name1)
+
+
+# In[23]:
+
+
+full_name = 'kushwanth'
+
+print(full_name)
+
+
+# In[24]:
+
+
+print(full_name.title())
+
+
+# In[25]:
+
+
+print(full_name.upper())
+
+
+# In[ ]:
+
+
+
+
