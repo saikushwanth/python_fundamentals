867bc30d57f7ac515c3c697cd89033a8941fb4d6
saikushwanth <67970353+saikushwanth@users.noreply.github.com>
2020-07-09 22:23:43 +0530

    Add files via upload

---
+++ Day3_B23_Python_fundamentals.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+first_name = 'sai'
+last_name = 'kushwanth'
+
+
+# In[10]:
+
+
+full_name = f"{first_name} {last_name}"
+
+print(full_name)
+
+
+# In[11]:
+
+
+#enhancement of the code 
+
+print(full_name.title())
+
+
+# In[12]:
+
+
+print(f"keep up the work, {full_name.title()}")
+
+
+# In[13]:
+
+
+print('fav_lang: pythonCC++')
+
+
+# In[14]:
+
+
+print('fav_lang: \npython\nC\nC++')
+
+
+# In[15]:
+
+
+print('fav_lang:\n\tpython\n\tC\n\tC++')
+
+
+# In[16]:
+
+
+#removing white spaces
+
+fav_lang = 'python '
+
+
+# In[17]:
+
+
+print(fav_lang)
+
+
+# In[18]:
+
+
+fav_lang.rstrip()
+
+
+# In[19]:
+
+
+fav_lang = ' python'
+
+
+# In[20]:
+
+
+print(fav_lang)
+
+
+# In[21]:
+
+
+fav_lang.lstrip()
+
+
+# In[22]:
+
+
+fav_lang = ' python '
+
+
+# In[23]:
+
+
+print(fav_lang)
+
+
+# In[24]:
+
+
+fav_lang.strip()
+
+
+# In[25]:
+
+
+import this
+
+
+# In[ ]:
+
+
+
+
